f9c43d6417db6b8263e2d4fa2d3204a735c16b12
soybean2 <97391783+soybean2@users.noreply.github.com>
2023-04-13 10:41:14 +0800

    修改test.java

--- src/com/ChargeCalculation/Test.java
+++ src/com/ChargeCalculation/Test.java
@@ -4,9 +4,12 @@ public class Test {
     public static void main(String[] args) {
         Bus bus = new Bus();
         System.out.println(bus.receive());
+        System.out.println("--------------");
         Taxi taxi = new Taxi(5);
         System.out.println(taxi.receive());
+        System.out.println("--------------");
         Cinema cinema = new Cinema();
         System.out.println(cinema.receive());
+        System.out.println("--------------");
     }
 }
